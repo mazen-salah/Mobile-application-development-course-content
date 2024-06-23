06b036db7ed1e991d929c90aab8b3059bca52348
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-06-23 23:23:18 +0000

    feat: Add user input functionality to FacultyDataCollection

--- Session 2/0. Lab 1 solution/Task 2: Faculty Data Collection/FacultyDataCollection.java
+++ Session 2/0. Lab 1 solution/Task 2: Faculty Data Collection/FacultyDataCollection.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class FacultyDataCollection {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String facultyName;
+        char lastGrade;
+        float lastDegree;
+        int subjectsCount;
+        boolean passed;
+
+        System.out.println("Enter faculty name: ");
+        facultyName = sc.nextLine();
+        System.out.println("Enter last grade: ");
+        lastGrade = sc.next().charAt(0);
+        System.out.println("Enter last degree: ");
+        lastDegree = sc.nextFloat();
+        System.out.println("Enter number of subjects: ");
+        subjectsCount = sc.nextInt();
+        System.out.println("Enter passed status: ");
+        passed = sc.nextBoolean();
+
+        System.out.println("Faculty name: " + facultyName);
+        System.out.println("Last grade: " + lastGrade);
+        System.out.println("Last degree: " + lastDegree);
+        System.out.println("Number of subjects: " + subjectsCount);
+        System.out.println("Passed status: " + passed);
     }
 }
