d3b728571af8467b7f85e4bdc3bb27f85c2af1cc
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-06-24 16:56:51 +0000

    Add if statement code examples and documentation

---
+++ Session 2/2. if statement/example.java
@@ -0,0 +1,12 @@
+public class example {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 5) {
+            System.out.println("x is greater than 5");
+        } else if (x < 5) {
+            System.out.println("x is less than 5");
+        } else {
+            System.out.println("x is equal to 5");
+        }
+    }
+}
