6c9c0dcc6ad371981dbbf74ef5621671cf6f45a7
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-06 13:53:58 +0000

    feat: Add summation program using a for loop

---
+++ Session 3/5. Applications on loops/summation.java
@@ -0,0 +1,11 @@
+public class summation {
+    public static void main(String[] args) {
+        int sum = 0;
+        
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        
+        System.out.println("Sum: " + sum);
+    }
+}
