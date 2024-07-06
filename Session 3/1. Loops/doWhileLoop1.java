e31a6a14d7e46370c72ed4e6768a974f92d1ffe7
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-06 13:43:39 +0000

    feat: Add do-while loop example in doWhileLoop1.java

---
+++ Session 3/1. Loops/doWhileLoop1.java
@@ -0,0 +1,8 @@
+public class doWhileLoop1 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+        } while (i++ < 10);
+    }
+}
