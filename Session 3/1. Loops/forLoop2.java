eaf430625a3f22d918d707b1cbd9d06dcf40101f
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-06 12:37:40 +0000

    feat: Improve forLoop2.java with number validation and range printing

--- Session 3/1. Loops/forLoop2.java
+++ Session 3/1. Loops/forLoop2.java
@@ -10,17 +10,20 @@ public class forLoop2 {
         int num2 = sc.nextInt();
         System.out.println();
 
-        if (num2<num1) {
+        // Swap the numbers if the second number is smaller than the first number
+        if (num2 < num1) {
             int temp = num1;
             num1 = num2;
             num2 = temp;
         }
+        // If both numbers are equal, print a message and exit the program
         else if (num1 == num2) {
             System.out.println("Both numbers are equal.");
             sc.close();
             return;
         }
         
+        // Print the range of numbers between num1 and num2 (inclusive)
         for (int i = num1; i <= num2; i++) {
             System.out.println(i);
         }
