46b9cd21510564db764558448fcdef4eccd67da3
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-06 14:52:14 +0000

    Refactor prime number checking program to optimize loop condition

--- Session 3/5. Applications on loops/prime.java
+++ Session 3/5. Applications on loops/prime.java
@@ -7,7 +7,9 @@ public class prime {
         int n = sc.nextInt();
 
         boolean isPrime = true;
-        for (int i = 2; i < n; i++) {
+        // for (int i = 2; i <= n; i++) {
+        // for (int i = 2; i <= n/2; i++) {
+        // for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 isPrime = false;
                 break;
@@ -19,6 +21,6 @@ public class prime {
         } else {
             System.out.println("Not Prime");
         }
-    sc.close();
+        sc.close();
     }
 }
