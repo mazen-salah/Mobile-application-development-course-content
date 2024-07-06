2c8e29d3351d57957e76b816af3c5b9c355d5b0d
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-06 14:19:32 +0000

    Refactor factorial program to include user input prompt and display the result

--- Session 3/5. Applications on loops/factorial.java
+++ Session 3/5. Applications on loops/factorial.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
         int n = sc.nextInt();
         int fact = 1;
         for (int i = 1; i <= n; i++) {
             fact = fact * i;
         }
-        System.out.println(fact);
+        System.out.println("Factorial of " + n + " is " + fact);
         sc.close();
     }
 }
