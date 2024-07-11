9cb12f2d6af9559a9de4ec38fbf39ef8a9f02ff8
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-11 04:23:41 +0000

    feat: Add user input prompts for factorial and exponential programs

--- Session 4/0. Lab 3 solution/Task 5: factorial/factorial.java
+++ Session 4/0. Lab 3 solution/Task 5: factorial/factorial.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number you want to find the factorial of: ");
         int n = sc.nextInt();
         int fact = 1;
         for (int i = 1; i <= n; i++) {
--- Session 4/0. Lab 3 solution/Task 6: exponential/exponential.java
+++ Session 4/0. Lab 3 solution/Task 6: exponential/exponential.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class exponential {
      public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the power you want to raise the number to: ");
             int n = sc.nextInt();
+            System.out.println("Enter the number you want to raise to the power of " + n + ": ");
             int x = sc.nextInt();
             int exp = 1;
             for (int i = 1; i <= n; i++) {
