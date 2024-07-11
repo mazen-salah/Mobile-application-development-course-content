3874a71c08a84aa859ec225ad715c91b7de5b5cc
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-11 04:48:02 +0000

    Refactor nested loops for improved readability and efficiency

--- Session 4/2. Arrays/example.java
+++ Session 4/2. Arrays/example.java
@@ -1,3 +1,18 @@
 public class example {
-    
+    public static void main(String[] args) {
+        int[] arr = { 12, 1, 2, 42, 5 };
+        int max = arr[0];
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+
+    }
 }
