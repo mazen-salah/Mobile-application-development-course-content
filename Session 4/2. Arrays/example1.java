17b9625a40637354f15c54b5cf7bfb608414d8cf
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-11 04:54:16 +0000

    feat: Add examples for iterating over an array and accessing specific elements
    The code changes include two new examples, example1.java and example2.java, that demonstrate how to iterate over an array and access specific elements. These examples can be used as a reference for working with arrays in Java.

---
+++ Session 4/2. Arrays/example1.java
@@ -0,0 +1,8 @@
+public class example1 {
+    public static void main(String[] args) {
+        String[] names = { "John", "Doe", "Jane", "Doe", "Smith", "Johnson", "Brown", "Davis", "Miller", "Wilson"};
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
---
+++ Session 4/2. Arrays/example2.java
@@ -0,0 +1,7 @@
+public class example2 {
+    public static void main(String[] args) {
+        String[] names = { "John", "Doe", "Jane", "Doe", "Smith", "Johnson", "Brown", "Davis", "Miller", "Wilson" };
+        System.out.println(names[3]);
+        System.out.println("Length: " + names.length);
+    }
+}
