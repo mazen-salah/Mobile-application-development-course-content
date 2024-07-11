8d986051dbb166e9088bdc45c2bada46c2760416
Mazen Tamer Salah <78306991+mazen-salah@users.noreply.github.com>
2024-07-11 05:06:32 +0000

    feat: Calculate the sum of elements in an array
    This commit adds code to calculate the sum of elements in an array. The `example5.java` file contains the implementation.

---
+++ Session 4/2. Arrays/example5.java
@@ -0,0 +1,9 @@
+public class example5 {
+    public static void main(String[] args) {
+        int[] numbers = { 12, 34, 56, 78, 90, 32, 54, 76, 98, 10 };
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+    }
+}
